0af766ecbd7bdf5566045449e0bdeed0accbe17c
S. M. V. <117322465+smv1256@users.noreply.github.com>
2025-03-09 16:32:53 -0400

    config, package cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    async headers() {
+        return [
+          {
+            source: "/api/:path*",
+            headers: [
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+            ],
+          },
+        ];
+    },
+};
 
 export default nextConfig;
+  
