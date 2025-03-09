810b27c67ce99d102b4f775bc69869aacd6ec5aa
S. M. V. <117322465+smv1256@users.noreply.github.com>
2025-03-09 16:30:07 -0400

    pages exist

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,8 +1,8 @@
-export default function Home () {
+export default function Support () {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-1 flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold">Home</h1>
+        <h1 className="text-6xl font-bold">Support</h1>
       </main>
     </div>
   );
