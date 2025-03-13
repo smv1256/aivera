7712e8708ad86e21084b969bfcaae2db0d218430
S. M. V. <117322465+smv1256@users.noreply.github.com>
2025-03-12 22:08:53 -0400

    dashboard redirect for error, "Not Aurhenticated"

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,22 @@
-import getServerSession from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+    import { auth } from "../../../auth";
+    import { redirect } from "next/navigation";
 
-export default async function Dashboard () {  
-    const session = getServerSession(authOptions);
-    console.log(session);
-
-    if (!session) return <div>Not authenticated</div>
-
-    return (
-      <div className="flex flex-col min-h-screen">
-        <main className="flex-1 flex flex-col items-center justify-center">
-          <h1 className="text-6xl font-bold">Dashboard</h1>
-        </main>
-      </div>
-    );
-  }
-  
+    export default async function Dashboard() {
+      const session = await auth();
+    
+      console.log("Session data:", session); // Debugging
+    
+      if (!session || !session.user) {
+        redirect("/login");
+        // return <div>Not authenticated</div>;
+      }
+    
+      return (
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-1 flex flex-col items-center justify-center">
+            <h1 className="text-6xl font-bold">Dashboard</h1>
+            <p>{session.user.name}</p>
+          </main>
+        </div>
+      );
+    }    
