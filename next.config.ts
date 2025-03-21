a8d11b4a315294247315ba641002e3342621aba8
S. M. V. <117322465+smv1256@users.noreply.github.com>
2025-03-21 18:52:37 -0400

    vsc debugger

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
           },
         ];
     },
+    webpack: (config, { dev }) => {
+      if (dev) {
+          config.devtool = "source-map";
+      }
+      return config;
+  },
 };
 
 export default nextConfig;
